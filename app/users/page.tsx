d1b45358f6003a236ac6cafdd7219e332c44fb4d
navjotbetasoft <navjotkaur.betasoft@outlook.com>
2026-02-25 10:19:29 +0530

    changes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import DxGrid from "../Components/common/DxGrid";
 import { columns, data } from "./constants";
 
 export default function UsersPage() {
+  console.log("hello");
   return (
     <>
       <DxGrid data={data} columns={columns} users="Users" />
